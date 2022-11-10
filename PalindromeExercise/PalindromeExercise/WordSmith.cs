848860f877349b448fd000100257e23e8e7fc90e
kyle jenne <strongagent1@gmail.com>
2022-11-10 12:18:19 -0500

    Palindrome exercise for KJ

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {        
+        public bool IsAPalindrome(string word)
+        {
+            var small = word.ToLower();
+            var back = "";
+            for (int i = small.Length -1; i >= 0; i--)
+            {
+                back += small[i];
+            }
+            if (back == small)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }//In your WordSmith class, you will create a method (IsAPalindrome) that will take a parameter of type string 
+    //and return a value of type bool. If the given input is a palindrome – return true, else return false. 
+    //You will finish this method after you write your tests.
+    //Write you test method in the UnitTest1.cs file - located in the PalindromeExerciseTests project file
+    
+
+    //In UnitTest1.cs, remove the Fact portion and replace it with:
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,29 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        
+        
+        public class PalTest
         {
+            [Theory]
+            [InlineData("racecar", true)]
+            [InlineData("Racecar", true)]
+            [InlineData("kayak", true)]
+            [InlineData("hannah", true)]
+            [InlineData("banana", false)]
+            [InlineData("test", false)]
+            public void IsAPalindromeTest(string word, bool expected)
+            {
+                //arrange
+                var ws = new WordSmith();
+                //act
+                var actual = ws.IsAPalindrome(word);
+                //assert
+                Assert.Equal(expected, actual);
 
+            }  
+                
         }
-    }
+    }   
+
 }
